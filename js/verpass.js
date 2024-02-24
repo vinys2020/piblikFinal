587f03a4c70383d1ea2db3f4ddc910751438650a
Facundo <faculez07@gmail.com>
2024-02-24 17:23:33 -0300

    fix: eye, navbar

--- js/verpass.js
+++ js/verpass.js
@@ -2,22 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const passwordInput = document.getElementById("password");
   const togglePasswordButton = document.getElementById("togglePassword");
   const confirmPasswordInput = document.getElementById("confirmPassword");
-  const oldPasswordInput = document.getElementById("oldPassword");
-
-  
-  const toggleConfirmPasswordButton = document.getElementById(
-    "toggleConfirmPassword"
-  );
+  const toggleConfirmPasswordButton = document.getElementById("toggleConfirmPassword");
 
   togglePasswordButton.addEventListener("click", function () {
     togglePasswordVisibility(passwordInput, togglePasswordButton);
   });
 
-
+  toggleConfirmPasswordButton.addEventListener("click", function () {
+    togglePasswordVisibility(confirmPasswordInput, toggleConfirmPasswordButton);
+  });
 
   function togglePasswordVisibility(inputField, toggleButton) {
-    const type =
-      inputField.getAttribute("type") === "password" ? "text" : "password";
+    const type = inputField.getAttribute("type") === "password" ? "text" : "password";
     inputField.setAttribute("type", type);
 
     // Cambiar el ícono del botón
